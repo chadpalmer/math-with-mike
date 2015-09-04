4ced148f1e035863f12cba498a5777b1c797d003
Selah...hey nice try! <theshadowzone12@gmail.com>
2015-09-04 12:44:27 -0500

    Added audio files
    Added audio files, code was done by MikeAsMike I'm just adding it to
    github.

--- mathGameOne.js
+++ mathGameOne.js
@@ -1,52 +1,25 @@
 //Widget controls
 
-var lastFunction;
-var getAnswer;
-var sum;
-var difference;
-var product;
-var numOne;
-var numTwo;
-
-function showProbSet() {
-	document.getElementById("probSet").style.display = 'block';
-};
-
-function hideProbSet() {
-	document.getElementById("probSet").style.display = 'none';
-};
-
-function showAnswerSet() {
-	document.getElementById("answerSet").style.display = 'block';
-};
-
-function hideAnswerSet() {
-	document.getElementById("answerSet").style.display = 'none';
-};
+var lastFunction, getAnswer, sum, difference, product, quotient, numOne, numTwo, rightAud, wrongAud;
 
 function getFocus() {
 	document.getElementById("answerSet").focus();
-};
-
-function showEnter() {
-	document.getElementById("buttonEnter").style.display = 'block';
-};
-
-function hideEnter() {
-	document.getElementById("buttonEnter").style.display = 'none';
-};
+}
 
 function showQuit() {
 	document.getElementById("quit").style.display = 'block';
-};
-
-function hideQuit() {
-	document.getElementById("quit").style.display = 'none';
-};
+}
 
 function onQuit() {
 	window.location.reload();
-};
+}
+
+function switchOperation() {
+	document.getElementById("probSet").style.display = 'block';
+	document.getElementById("answerSet").style.display = 'block';
+	document.getElementById("buttonEnter").style.display = 'block';
+	getFocus();
+}
 
 function checkAnswer() {
 	if (lastFunction == "doAdd") {
@@ -55,10 +28,21 @@ function checkAnswer() {
 		checkSub();
 	} else if (lastFunction == "doMult") {
 		checkMult();
-	} else {
+	} else if (lastFunction == "doDiv") {
 		checkDiv();
 	}
-};
+}
+
+function playRight() {
+	rightAud = document.getElementById("rightAud");
+	rightAud.play();
+}
+
+function playWrong() {
+	wrongAud = document.getElementById("wrongAud");
+	wrongAud.play();
+}
+
 
 //Addition function
 function doAdd() {
@@ -71,30 +55,28 @@ function doAdd() {
 	} else {
 		document.getElementById("probSet").innerHTML = (numTwo + " + " + numOne + " =");
 	}
-	
-	showProbSet();
-	showAnswerSet();
-	showEnter();
-	getFocus();
-	
 	lastFunction = "doAdd";
-};
+
+	switchOperation();
+}
 
 function checkAdd() {
 	getAnswer = document.getElementById("answerSet").value;
 	
 	if (sum == getAnswer){
+		playRight();
 		alert("That is correct!");
 		document.getElementById("answerSet").value = '';
 		doAdd();
 	} else {
+		playWrong();
 		alert("Oops, wrong answer.  Try again.");
 		document.getElementById("answerSet").value = '';
 		getFocus();
 	}
 	
 	showQuit();
-};
+}
 
 //Subtraction function
 function doSub() {
@@ -108,30 +90,28 @@ function doSub() {
 		document.getElementById("probSet").innerHTML = (numTwo + " - " + numOne + " =");
 		difference = numTwo - numOne;
 	}
-	
-	showProbSet();
-	showAnswerSet();
-	showEnter();
-	getFocus();
-	
 	lastFunction = "doSub";
-};
+
+	switchOperation();
+}
 
 function checkSub() {
 	getAnswer = document.getElementById("answerSet").value;
 	
 	if (difference == getAnswer){
+		playRight();
 		alert("That is correct!");
 		document.getElementById("answerSet").value = '';
 		doSub();		
 	} else {
+		playWrong();
 		alert("Oops, wrong answer.  Try again.");
 		document.getElementById("answerSet").value = '';
 		getFocus();
 	}
 	
 	showQuit();
-};
+}
 
 function doMult() {
 	numOne = Math.floor((Math.random() *10) +1);
@@ -143,64 +123,53 @@ function doMult() {
 	} else {
 		document.getElementById("probSet").innerHTML = (numTwo + " x " + numOne + " =");
 	}
-	
-	showProbSet();
-	showAnswerSet();
-	showEnter();
-	getFocus();
-	
 	lastFunction = "doMult";
-};
+
+	switchOperation();
+}
 
 function checkMult() {
 	getAnswer = document.getElementById("answerSet").value;
 	
 	if (product == getAnswer) {
+		playRight();
 		alert("That is correct!");
 		document.getElementById("answerSet").value = '';
 		doMult();
 	} else {
+		playWrong();
 		alert("Oops, wrong answer.  Try again.");
 		document.getElementById("answerSet").value = '';
 		getFocus();
 	}
 	
 	showQuit();
-};
+}
 
 function doDiv() {
-	numOne = Math.floor((Math.random() *10) +1);
 	numTwo = Math.floor((Math.random() *10) +1);
+	numOne = numTwo * Math.floor((Math.random() *10) +1);
 	quotient = numOne / numTwo;
-	
-	if (quotient % numTwo === 0) {
-		document.getElementById("probSet").innerHTML = (numOne + " / " + numTwo + " =");		
-	} else {
-		doDiv();	
-	}
-	
-	showProbSet();
-	showAnswerSet();
-	showEnter();
-	getFocus();
-	
+	document.getElementById("probSet").innerHTML = (numOne + " / " + numTwo + " =");
 	lastFunction = "doDiv";
 	
-};
+	switchOperation();
+}
 
 function checkDiv() {
 	getAnswer = document.getElementById("answerSet").value;
 	
 	if (quotient == getAnswer) {
+		playRight();
 		alert("That is correct!");
 		document.getElementById("answerSet").value = '';
 		doDiv();
 	} else {
+		playWrong();
 		alert("Oops, wrong answer.  Try again.");
 		document.getElementById("answerSet").value = '';
 		getFocus();
 	}
 	
 	showQuit();
-};
-
+}
